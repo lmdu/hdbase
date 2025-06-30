6edd7bfb620fbcf4a06b24cddd0f47fb7bad058a
Lianming Du <adullb@qq.com>
2025-06-30 22:47:49 +0800

    update

--- hdbase/filters.py
+++ hdbase/filters.py
@@ -1,8 +1,37 @@
-from django_filters import FilterSet
+from django import forms
+from django_filters import *
 
 from .models import *
 
 class CardiomyopathyDiseaseFilter(FilterSet):
+	patient__name = CharFilter(
+		label = "患者姓名",
+		lookup_expr='icontains',
+		widget = forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+	)
+	patient__number = CharFilter(
+		label = "患者登记号",
+		lookup_expr='icontains',
+		widget = forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+	)
+	disease_type = CharFilter(
+		label = "心肌病分型",
+		lookup_expr='icontains',
+		widget = forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+	)
+
+	mutate_gene = CharFilter(
+		label = "突变基因",
+		lookup_expr='icontains',
+		widget = forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+	)
+
+	arrhythmia_type = CharFilter(
+		label = "心律失常类型",
+		lookup_expr='icontains',
+		widget = forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+	)
+
 	class Meta:
 		model = CardiomyopathyDisease
-		fields = ['disease_type', 'mutate_gene', 'arrhythmia_type']
+		fields = ['patient__name', 'patient__number', 'disease_type', 'mutate_gene', 'arrhythmia_type']
